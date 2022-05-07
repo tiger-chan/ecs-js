4790184294773781ac545f977506709aa9bdf568
Anthony <anthony.r.young@gmail.com>
2022-05-07 16:38:54 -0600

    Add Fixed array tests

--- src/fixed_array.js
+++ src/fixed_array.js
@@ -1,45 +1,53 @@
 import { Vector } from "./stl.js";
 
 /**
- * @constructor
- * @param {number} size
+ * @template T
+ * @implements {std.FixedArray<T>}
  */
-export function FixedArray(size) {
-	/** @type {std.FixedArray<any>} */
-	// @ts-ignore
-	let a = new Vector(size);
+export class FixedArray extends Array {
+	constructor(arrayLength = 1) {
+		super(arrayLength)
 
-	/** @this {std.FixedArray<any>} */
-	let resize = function (newSize) {
-		if (this.__size < newSize && newSize < this.__size) {
-			this.__size = newSize;
-		}
-		return this.__size;
-	};
-
-	/** @this {std.Vector<any>} */
-	let newPush = function (x) {
-		return this[this.__size++] = x, this.__size;
-	};
-
-	/** @this {std.Vector<any>} */
-	let newPop = function () {
-		if (0 < this.__size) {
-			this[--this.__size] = undefined;
-		}
-	};
-
-	a.resize = resize.bind(a);
-	a.push = newPush.bind(a);
-	a.pop = newPop.bind(a);
-
-	if (Object.seal) {
-		// fill array with some value because
-		// empty slots can not be changed after calling Object.seal
-		a.fill(undefined);
-
-		Object.seal(a);
-		// now a is a fixed-size array with mutable entries
+		this.fill();
+		this.__capacity = arrayLength;
+		Object.seal(this);
 	}
-	return a;
+
+	back() {
+		if (0 < this.__size) {
+			return this[this.__size - 1];
+		}
+		return null;
+	}
+
+	resize(newSize) {
+		return this.__size = Math.max(Math.min(newSize, this.__capacity), 0);
+	}
+
+	capacity() {
+		return this.__capacity;
+	}
+
+	size() {
+		return this.__size;
+	}
+
+	push(x) {
+		if (this.__size < this.length) {
+			return this[this.__size++] = x, this.__size;
+		}
+	}
+
+	pop() {
+		if (0 < this.__size) {
+			let a = this[--this.__size];
+			return this[this.__size] = undefined, a;
+		}
+		return undefined;
+	}
+
+	__size = 0;
+	__capacity = 0;
 }
+
+export default FixedArray;
